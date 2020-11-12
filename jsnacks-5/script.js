3bdd4333da07c6de0f9613ad4491695913241b5b
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-11-12 16:34:29 +0100

    JSNACK- ES6 - jsnack 5

--- jsnacks-5/script.js
+++ jsnacks-5/script.js
@@ -9,6 +9,26 @@ che hanno la posizione compresa tra i due numeri
 inseriti dall’utente
 */
 
-var myArray = ['Michele', 'Fabio','Roberto', 'Giovanni', 'Simone', 'Chiara']
-var fromNum = parseInt(prompt(`inserisci un numero tra 0 e ${myArray.length}` ))
-var toNum = parseInt(prompt(`inserisci un numero tra 0 e ${myArray.length}` ))
+const myArray = ['Michele', 'Fabio','Roberto', 'Giovanni', 'Simone', 'Chiara'];
+
+/*chiediamo all’utente con 2 prompt due numeri
+tra 0 e la lunghezza dell’array */
+
+var fromNum = parseInt(prompt(`inserisci un numero tra 0 e ${myArray.length}`));
+var toNum = parseInt(prompt(`inserisci un numero tra 0 e ${myArray.length}`));
+
+/* quindi creiamone uno nuovo con solo i valori
+che hanno la posizione compresa tra i due numeri
+inseriti dall’utente */
+
+const newArray= [];
+for (let i = fromNum; i < toNum; i++) {
+    let name = myArray[i];
+    newArray.push(name);
+}
+
+// Stampare
+
+newArray.forEach(element) => {
+  $('#result-5').append( element );
+}
